e5c6344965edce9f32f83952e17fba1dfe79a90f
Mitchell Currie <mitch@mitchellcurrie.com>
2025-11-24 13:57:32 +1100

    Fix darkmode

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
 
-import "./main.css"
-import "./styles/theme.css"
 import "./index.css"
+import "./styles/theme.css"
+import "./main.css"
 
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
